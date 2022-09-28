383dc775ed95df06a7c5674ffbc4221dd68f9c9e
luishenriqueabc <thais.liana2011@gmail.com>
2022-09-28 15:11:31 -0300

    Sistema funcionando

--- src/pages/PaginaContas.js
+++ src/pages/PaginaContas.js
@@ -1,63 +1,69 @@
+import {useRef, useState} from 'react'
 import './PaginaContas.css';
-import {useState, useEffect} from 'react';
-import Formulario from '../components/Formulario';
 
-const PaginaContas = () => {
-  const [produto, setproduto] = useState(null);
-  useEffect(() => {
-  fetch("http://localhost/pj2/api/produto/select-all")
-  .then((response) => response.json())
-  .then((data) => setproduto(data));        
-  }, []);
-  return (
-  <>
- 
-  <h1> ADMIN produto</h1>
-  <div className="form">
- 
-  </div>
-  <div className='cs'>
-  {produto &&
-    produto.map((produto) => {
-    return (
-      <div key={produto.id}>
-      <div className='card'>
-   
-     
-      <div className='cardinfo'>
-        <div className='nome'>
-        <h1>{produto.nome}</h1>
-        </div>
-        {produto.quantidade} * {produto.quantidade} = 
+const Contas = () => {
 
-        </div>
-        <div className='pertence'>
-        <h3>Pertence a qual cidade?</h3>
-        <p>{produto.pertence}</p>
-        </div>
-        <div className='quantas'>
-        <h3>Quantas pessoas vão até lá?</h3>
-        <p>{produto.quantaspessoas}</p>
-        </div>
-        <div className='queriratela'>
-        <h3>Quer ir até lá?</h3>
-        <div className='Sim'>
-        <a href='https://www.decolar.com/pacotes/rio/pacotes-para-rio+de+janeiro?package_id=be8e62e9a1e10324e912bc3b3c22f49988055599591348c7598521b487e110fd&clickedPrice=BRL_1137&priceDate=1655724010474&searchId=d5036125d93c4692a56a44c822aeabe4'> <p>Quero!</p></a>
-        </div>
-        </div>
-        <div className='comentariosss'>
-          <h1>Comentarios</h1>
-        </div>
-    
-        </div>
-        </div>  
-          )
-        })
+const [lucro, setLucro] = useState()
+
+  let nomeRef = useRef();
+  let quantidadeRef = useRef();
+  let precoRef = useRef();
+  let investimentoRef = useRef();
+ 
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    let nome = event.target[0].value
+    let quantidade = event.target[1].value
+    let preco = event.target[2].value
+    let investimento = event.target[3].value
+
+    let valorTotal = quantidade * preco
+    let calculoLucro = valorTotal - investimento
+
+    let mensage = `O ${nome}, teve ${calculoLucro} de Lucro`
+
+    console.log(mensage)
+
+    setLucro(calculoLucro)
+
+    const formData = new FormData();
+    formData.append('nome', event.target[0].value);
+    formData.append('quantidade', event.target[1].value);
+    formData.append('preco', event.target[2].value);
+    formData.append('investimento', event.target[3].value);
+    formData.append('lucro', calculoLucro);
+
+    fetch(
+      "http://localhost/pj2/api/produto/create",
+      {
+        method: 'POST',
+        body: formData,  
       }
-      </div>
-     
-      </>
-    )
+      )
+      .then((response) => response.json())
+      .then((data) => {
+        nomeRef.current.value = ''
+        quantidadeRef.current.value = ''
+        precoRef.current.value = ''
+        investimentoRef.current.value = ''
+        alert(data.message)
+      });
   }
-
-export default PaginaContas;
+  return (
+    <div className="Formulario">
+     <h1>Cálculo de Lucros</h1>
+     <form onSubmit={(event) => handleSubmit(event)}>
+      <input placeholder='Nome' id='nome' type="text" ref={nomeRef}/>
+      <input placeholder='Quantidade' id='quant' type="number" ref={quantidadeRef}/>
+      <input placeholder='Valor Sugerido' id='valor' type="text" ref={precoRef}/>
+      <input placeholder='Investimento' id='invest' type="text" ref={investimentoRef}/>
+      <input placeholder='Calcular' type="submit" id='btn'/>
+     </form>
+      <p>Lucro: {lucro}</p>
+      
+    </div>
+  );
+}
+export default Contas;
